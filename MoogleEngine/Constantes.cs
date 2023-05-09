3c25eb93d282f41520f29cf49c1c1a08875a576c
GlenRios <glendariosrodriguez3@gmail.com>
2023-05-08 22:18:35 -0400

    terminado o eso creo

--- MoogleEngine/Constantes.cs
+++ MoogleEngine/Constantes.cs
@@ -2,8 +2,7 @@ namespace MoogleEngine;
 
 public static class Constantes
 {
-    public static char[] caracteresNoImportantes = "@ñ‘#!_*~@¿?¡!:/{}()[]<>'\"=-+,.;\\ \n".ToCharArray();
+    public static char[] caracteresNoImportantes = "@ñ‘#!_*~@¿?¡:^/{}()[]<>'\"=-+,.;\\ \n".ToCharArray();
     public static char[] caracteresNoImportantesII = "@ñ‘#_~@¿?¡:/{}()[]<>'\"=-+,.;\\ \n".ToCharArray();
-    public static char[] caracteresImportantes = "!*^".ToCharArray();
     public static string Direccion = @"..\Content";
 }
